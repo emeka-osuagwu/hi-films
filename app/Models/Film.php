e65b151fd571313f5c58354213c0d5bcd32684b7
Emeka <emekaosuagwuandela@gmail.com>
2018-06-11 13:04:36 +0100

    inital commit

---
+++ app/Models/Film.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 
+        'description', 
+        'realease_date',
+        'rating',
+        'ticket_price',
+        'country',
+        'genre',
+        'photo',
+    ];
+}
