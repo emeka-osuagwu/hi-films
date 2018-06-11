2eaf97d2b7bc1eb904e92a1bc929aed5b268a408
Emeka <emekaosuagwuandela@gmail.com>
2018-06-11 13:24:11 +0100

    add FilmController

---
+++ app/Http/Controllers/Api/FilmController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+class FilmController extends Controller
+{
+    public function FunctionName($value='')
+    {
+    	return 1;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,19 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Films Routes
+|--------------------------------------------------------------------------
+| {url}/films/*
+*/
+Route::group(['prefix' => 'films'], function () {
+	Route::get('/', 'Api\FilmController@FunctionName');
+	Route::post('login', 'Api\Auth\LoginController@posUsertLogin');
+	Route::post('create', 'Api\UserController@postCreateUser');
+	Route::post('update', 'Api\UserController@postUpdateUser');
+});
+
